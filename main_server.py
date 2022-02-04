119acc42d6ce41b2ebb701a5cc0f831178d96416
Savely1332 <98023381+Savely1332@users.noreply.github.com>
2022-02-04 15:23:47 +0300

    Add files via upload

--- main_server.py
+++ main_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, abort, flash, redirect
 
 
 app = Flask(__name__)
@@ -16,6 +16,19 @@ def begin():
 def index():
     username = 'Stolyarov'
     return render_template('index.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        for user in users:
+            if request.form['username'] == user['username']:
+                if request.form['password'] == user['password']:
+                    return redirect(f"/profile/{user['username']}")
+                else:
+                    flash('Неправильный пароль', category='error')
+                    break
+            else:
+                flash('Неправильный логин', category="error")
 
 
 @app.route('/day-<num>')
@@ -44,6 +57,18 @@ def form():
             print(item , request.form[item])
     return render_template('form.html')
 
+@app.route('/profile/<username>')
+def profile(username):
+    for user in users:
+        if user['username'] == username:
+            if 'logged_in' in session:
+                if session['logged_in'] == username:
+                    return render_template('profile.html', username=username)
+                else:
+                    abort(403)
+        flash('Вам туда нельзя. Залогинтесь' , category="error")
+        return redirect(url_for("login"))
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
